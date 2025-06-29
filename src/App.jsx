485c672e98c06648907bc2be14c89dd82a7b9ffb
Mhl86 <Mallaham1986@gmail.com>
2025-06-30 01:52:37 +0200

    localStorage and showing the card in the page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,19 @@ const App = () => {
   return (
     <>
       <Header />
+      <div className="flex flex-wrap justify-center gap-6 px-4 ">
+        {JSON.parse(localStorage.getItem("diaryEntries"))?.map(
+          (entry, index) => (
+            <div className=" p-4 mb-4 rounded shadow-lg w-[40%]" key={index}>
+              <h3>{entry.title}</h3>
+              <p> {entry.status}</p>
+              <p>Date: {new Date(entry.date).toLocaleDateString()}</p>
+              {entry.photo && <img src={entry.photo} alt="Diary" />}
+              <p>{entry.content}</p>
+            </div>
+          )
+        )}
+      </div>
     </>
   );
 };
--- src/header.jsx
+++ src/header.jsx
@@ -11,6 +11,35 @@ function Header() {
     setIsClicked(false);
   }
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const data = {
+      title: document.querySelector('input[placeholder="Title"]').value,
+      status: document.querySelector('input[name="status"]:checked').value,
+      date: document.querySelector('input[type="date"]').value,
+      photo: document.querySelector('input[type="url"]').value,
+      content: document.querySelector('textarea[placeholder="description"]')
+        .value,
+    };
+    function saveToLocal() {
+      const diaryEntry = {
+        title: data.title,
+        status: data.status,
+        date: data.date,
+        photo: data.photo,
+        content: data.content,
+      };
+
+      const existingEntries =
+        JSON.parse(localStorage.getItem("diaryEntries")) || [];
+      existingEntries.push(diaryEntry);
+      localStorage.setItem("diaryEntries", JSON.stringify(existingEntries));
+    }
+
+    saveToLocal();
+    closePopup();
+  };
   return (
     <>
       <header className="bg-red-300 p-4 popup">
@@ -28,6 +57,20 @@ function Header() {
       >
         Add
       </button>
+      <div className="flex  items-center p-4 mt-20 gap-20 justify-center">
+        <label className=" text-xl" htmlFor="date">
+          Search By Date
+        </label>
+        <input
+          className="border border-red-300 p-2 rounded w-[30%]"
+          type="date"
+          name="date"
+          id="date"
+        />
+        <button className="bg-red-300 p-2 rounded w-[20%] max-w-40">
+          Search
+        </button>
+      </div>
       {isClicked && (
         <div className="border-red-300 p-6 rounded shadow-lg fixed w-[50%] max-w-200  h-160 bg-white z-50  items-center justify-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
           <h2 className="text-2xl font-semibold mb-4 bg-red-300 text-center p-4">
@@ -36,7 +79,7 @@ function Header() {
 
           <form
             className="flex flex-col gap-4 justify-around "
-            onSubmit={closePopup}
+            onSubmit={handleSubmit}
           >
             <div className="flex  gap-2 justify-around">
               <label>Diary Title</label>
@@ -52,7 +95,7 @@ function Header() {
                 <input
                   type="radio"
                   name="status"
-                  value="todo"
+                  value="To Do"
                   className="w-3 h-3"
                 />
                 <span>To Do</span>
@@ -62,7 +105,7 @@ function Header() {
                 <input
                   type="radio"
                   name="status"
-                  value="done"
+                  value="Done"
                   className="w-3 h-3"
                 />
                 <span>Done</span>
@@ -96,7 +139,6 @@ function Header() {
             </div>
             <button
               className="bg-red-300  p-2 rounded w-[40%] mx-auto block mt-4"
-              onClick={closePopup}
               type="submit"
             >
               add and close
